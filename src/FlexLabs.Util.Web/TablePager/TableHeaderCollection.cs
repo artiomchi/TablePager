36d72825a7098e2ebe441cdf25788b87a0551851
Artiom Chilaru <artiom@flexlabs.org>
2016-01-30 11:36:18 +0000

    * Preparing for project migration to FlexLabs.Util.Web + Added TableHeaderCollection for better header initialisation + Added TableHeaderCollectionTests

---
+++ src/FlexLabs.Util.Web/TablePager/TableHeaderCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FlexLabs.Web.TablePager
+{
+    public class TableHeaderCollection : ICollection<ITableHeader>
+    {
+        private readonly IList<ITableHeader> _headers = new List<ITableHeader>();
+
+
+        public int Count { get { return _headers.Count; } }
+
+        public bool IsReadOnly { get { return false; } }
+
+        public void Add(ITableHeader item)
+        {
+            _headers.Add(item);
+        }
+
+        public void Add(String title)
+        {
+            _headers.Add(new TableHeader { Title = title });
+        }
+
+        public void Add(String title, Object value)
+        {
+            _headers.Add(new TableHeader { Title = title, Value = value });
+        }
+
+        public void Clear()
+        {
+            _headers.Clear();
+        }
+
+        public bool Contains(ITableHeader item)
+        {
+            return _headers.Contains(item);
+        }
+
+        public void CopyTo(ITableHeader[] array, int arrayIndex)
+        {
+            _headers.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<ITableHeader> GetEnumerator()
+        {
+            return _headers.GetEnumerator();
+        }
+
+        public bool Remove(ITableHeader item)
+        {
+            return _headers.Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _headers.GetEnumerator();
+        }
+    }
+}
---
+++ test/FlexLabs.Util.Web.Tests/TablePager/TableHeaderCollectionTests.cs
@@ -0,0 +1,41 @@
+﻿using FlexLabs.Web.TablePager;
+using System;
+using System.Linq;
+using System.Web;
+using Xunit;
+
+namespace FlexLabs.Util.Web.Tests.TablePager
+{
+    public class TableHeaderCollectionTests
+    {
+        [Fact]
+        public void TableHeaderCollection_InlineInit()
+        {
+            Func<Object, IHtmlString> customContent = x => new HtmlString("hello");
+
+            var headers = new TableHeaderCollection
+            {
+                { "Header 1" },
+                { "Header 2", 123 },
+                { new TableHeader { Title = "Header 3", CssClass = "css" } },
+                { new CustomTableHeader { Content = customContent } },
+            }.ToList();
+
+            Assert.Equal(4, headers.Count);
+
+            Assert.IsType<TableHeader>(headers[0]);
+            Assert.Equal("Header 1", (headers[0] as TableHeader).Title);
+
+            Assert.IsType<TableHeader>(headers[1]);
+            Assert.Equal("Header 2", (headers[1] as TableHeader).Title);
+            Assert.Equal(123, (headers[1] as TableHeader).Value);
+
+            Assert.IsType<TableHeader>(headers[2]);
+            Assert.Equal("Header 3", (headers[2] as TableHeader).Title);
+            Assert.Equal("css", (headers[2] as TableHeader).CssClass);
+
+            Assert.IsType<CustomTableHeader>(headers[3]);
+            Assert.Equal(customContent, (headers[3] as CustomTableHeader).Content);
+        }
+    }
+}
